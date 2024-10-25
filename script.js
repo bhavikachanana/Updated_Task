7b00510bc806007ae6e1fcb03193577538eec0ea
bhavikachanana <91448413+bhavikachanana@users.noreply.github.com>
2024-10-26 03:01:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,283 @@
+// var btnvar1=document.getElementById('btnh1');
+//        function Toggle1(){
+//         if(btnvar1.style.backgroundColor=="red" ){
+//             btnvar1.style.backgroundColor="white" 
+//         }
+//         else{
+//             btnvar1.style.backgroundColor="red"
+//         }
+//        }
+
+//        var btnvar2=document.getElementById('btnh2');
+//        function Toggle2(){
+//         if(btnvar2.style.backgroundColor=="red" ){
+//             btnvar2.style.backgroundColor="white" 
+//         }
+//         else{
+//             btnvar2.style.backgroundColor="red"
+//         }
+//        }
+
+//        var btnvar3=document.getElementById('btnh3');
+//        function Toggle3(){
+//         if(btnvar3.style.backgroundColor=="red" ){
+//             btnvar3.style.backgroundColor="white" 
+//         }
+//         else{
+//             btnvar3.style.backgroundColor="red"
+//         }
+//        }
+
+//        var btnvar4=document.getElementById('btnh4');
+//        function Toggle4(){
+//         if(btnvar4.style.backgroundColor=="red" ){
+//             btnvar4.style.backgroundColor="white" 
+//         }
+//         else{
+//             btnvar4.style.backgroundColor="red"
+//         }
+//        }
+
+//        var btnvar5=document.getElementById('btnh5');
+//        function Toggle5(){
+//         if(btnvar5.style.backgroundColor=="red" ){
+//             btnvar5.style.backgroundColor="white" 
+//         }
+//         else{
+//             btnvar5.style.backgroundColor="red"
+//         }
+//        }
+
+//        var btnvar6=document.getElementById('btnh6');
+//        function Toggle6(){
+//         if(btnvar6.style.backgroundColor=="red" ){
+//             btnvar6.style.backgroundColor="white" 
+//         }
+//         else{
+//             btnvar6.style.backgroundColor="red"
+//         }
+//        }
+
+//        var btnvar7=document.getElementById('btnh7');
+//        function Toggle7(){
+//         if(btnvar7.style.backgroundColor=="red" ){
+//             btnvar7.style.backgroundColor="white" 
+//         }
+//         else{
+//             btnvar7.style.backgroundColor="red"
+//         }
+//        }
+
+//        var btnvar8=document.getElementById('btnh8');
+//        function Toggle8(){
+//         if(btnvar8.style.backgroundColor=="red" ){
+//             btnvar8.style.backgroundColor="white" 
+//         }
+//         else{
+//             btnvar8.style.backgroundColor="red"
+//         }
+//        }
+
+//        var btnvar9=document.getElementById('btnh9');
+//        function Toggle9(){
+//         if(btnvar9.style.backgroundColor=="red" ){
+//             btnvar9.style.backgroundColor="white" 
+//         }
+//         else{
+//             btnvar9.style.backgroundColor="red"
+//         }
+//        }
+
+//        const plus= document.querySelector(".plus"),
+//        minus=document.querySelector(".minus"),
+//        num=document.querySelector(".num");
+       
+//        let a = 1;
+//        plus.addEventListener("click" , ()=>{
+//         a++;
+//         a=(a<10)?"0" + a: a;
+//         num.innerText=a;
+//         console.log(a)
+//        })
+
+//        minus.addEventListener("click" , ()=>{
+//         if(a>1){
+//             a--;
+//             a=(a<10)?"0" + a: a;
+//             num.innerText=a;
+//         }
+
+//        })
+
+//        const plu= document.querySelector(".plu"),
+//        minu=document.querySelector(".minu"),
+//        nu=document.querySelector(".nu");
+       
+//        let a1 = 1;
+//        plu.addEventListener("click" , ()=>{
+//         a1++;
+//         a1=(a1<10)?"0" + a1: a1;
+//         nu.innerText=a1;
+//         console.log(a1)
+//        })
+
+//        minu.addEventListener("click" , ()=>{
+//         if(a1>1){
+//             a1--;
+//             a1=(a1<10)?"0" + a1: a1;
+//             nu.innerText=a1;
+//         }
+
+//        })
+
+//        const pl= document.querySelector(".pl"),
+//        min=document.querySelector(".min"),
+//        n=document.querySelector(".n");
+       
+//        let a2 = 1;
+//        pl.addEventListener("click" , ()=>{
+//         a2++;
+//         a2=(a2<10)?"0" + a2: a2;
+//         n.innerText=a2;
+//         console.log(a2)
+//        })
+
+//        min.addEventListener("click" , ()=>{
+//         if(a2>1){
+//             a2--;
+//             a2=(a1<10)?"0" + a2: a2;
+//             n.innerText=a2;
+//         }
+
+//        })
+
+
+//        //sec
+
+//        const ab= document.querySelector(".ab"),
+//        bc=document.querySelector(".bc"),
+//        cd=document.querySelector(".cd");
+       
+//        let a3 = 1;
+//        ab.addEventListener("click" , ()=>{
+//         a3++;
+//         a3=(a3<10)?"0" + a3: a3;
+//         cd.innerText=a3;
+//         console.log(a3)
+//        })
+
+//        bc.addEventListener("click" , ()=>{
+//         if(a3>1){
+//             a3--;
+//             a3=(a3<10)?"0" + a3: a3;
+//             cd.innerText=a3;
+//         }
+
+//        })
+
+//        const de= document.querySelector(".de"),
+//        ef=document.querySelector(".ef"),
+//        fg=document.querySelector(".fg");
+       
+//        let a4 = 1;
+//        de.addEventListener("click" , ()=>{
+//         a4++;
+//         a4=(a1<10)?"0" + a4: a4;
+//         fg.innerText=a4;
+//         console.log(a1)
+//        })
+
+//        ef.addEventListener("click" , ()=>{
+//         if(a4>1){
+//             a4--;
+//             a4=(a4<10)?"0" + a4: a4;
+//             fg.innerText=a4;
+//         }
+
+//        })
+
+//        const gh= document.querySelector(".gh"),
+//        hi=document.querySelector(".hi"),
+//        ij=document.querySelector(".ij");
+       
+//        let a5 = 1;
+//        gh.addEventListener("click" , ()=>{
+//         a5++;
+//         a5=(a5<10)?"0" + a5: a5;
+//         ij.innerText=a5;
+//         console.log(ij)
+//        })
+
+//        hi.addEventListener("click" , ()=>{
+//         if(a5>1){
+//             a5--;
+//             a5=(a5<10)?"0" + a5: a5;
+//             ij.innerText=a5;
+//         }
+
+//        })
+//        //thu
+//        const jk= document.querySelector(".jk"),
+//        kl=document.querySelector(".kl"),
+//        lm=document.querySelector(".lm");
+       
+//        let a6 = 1;
+//        jk.addEventListener("click" , ()=>{
+//         a6++;
+//         a6=(a6<10)?"0" + a6: a6;
+//         lm.innerText=a6;
+//         console.log(a6)
+//        })
+
+//        kl.addEventListener("click" , ()=>{
+//         if(a6>1){
+//             a6--;
+//             a6=(a<10)?"0" + a6: a6;
+//             lm.innerText=a6;
+//         }
+
+//        })
+
+//        const mn= document.querySelector(".mn"),
+//        no=document.querySelector(".no"),
+//        op=document.querySelector(".op");
+       
+//        let a7 = 1;
+//        mn.addEventListener("click" , ()=>{
+//         a7++;
+//         a7=(a7<10)?"0" + a7: a7;
+//         op.innerText=a7;
+//         console.log(a7)
+//        })
+
+//        no.addEventListener("click" , ()=>{
+//         if(a7>1){
+//             a7--;
+//             a7=(a7<10)?"0" + a7: a7;
+//             op.innerText=a7;
+//         }
+
+//        })
+
+//        const pq= document.querySelector(".pq"),
+//        qr=document.querySelector(".qr"),
+//        rs=document.querySelector(".rs");
+       
+//        let a8 = 1;
+//        pq.addEventListener("click" , ()=>{
+//         a8++;
+//         a8=(a8<10)?"0" + a8: a8;
+//         rs.innerText=a8;
+//         console.log(a8)
+//        })
+
+//        qr.addEventListener("click" , ()=>{
+//         if(a8>1){
+//             a8--;
+//             a8=(a1<10)?"0" + a8: a8;
+//         rs.innerText=a8;
+//         }
+
+//        })
+    
+
